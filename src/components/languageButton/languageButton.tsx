e7cf1498a2f1ff327885fdf10b24e959806b6802
Mees van Wel <mees@hexa-it.nl>
2024-04-13 12:55:15 +0200

    Base language button

--- src/components/languageButton/languageButton.tsx
+++ src/components/languageButton/languageButton.tsx
@@ -1,22 +1,14 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import styles from "./languageButton.module.scss";
-import { SunIcon } from "../svg/sun";
-import { IconMoon } from "../svg/moon";
-import clsx from "clsx";
 import { NlFlag } from "../svg/nl";
 
 export const LanguageButton = component$(() => {
-  const dark = useSignal(false);
-
   return (
-    <div
-      class={clsx(styles.root)}
-      onClick$={() => {
-        dark.value = !dark.value;
-        document.body.setAttribute("data-theme", dark.value ? "dark" : "light");
-      }}
-    >
-      <NlFlag />
+    <div class={styles.root}>
+      <div class={styles.current}>
+        <NlFlag class={styles.flag} />
+      </div>
+      <div class={styles.dropdown}></div>
       {/* <div class={styles.track} />
       <div class={styles.icon}>
         <IconSun class={clsx(styles.iconSvg, styles.iconSun)} />
